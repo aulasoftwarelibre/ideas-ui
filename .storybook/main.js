651e4cd38442be7873e94204c00eb76f8bd87018
Sergio Gómez <sergio@uco.es>
2020-07-15 01:26:25 +0200

    Clean storybook configuration
    Fix load postcss with non-next tools

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,32 @@
+const path = require("path");
+
 module.exports = {
+  stories: ["../components/**/*.stories.tsx"],
   addons: [
     "@storybook/preset-typescript",
     "@storybook/addon-links",
     "@storybook/addon-knobs",
     "@storybook/addon-actions",
   ],
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: "postcss-loader",
+          options: {
+            ident: "postcss",
+            plugins: [
+              require("postcss-preset-env"),
+              require("tailwindcss")("./tailwind.config.js"),
+            ],
+          },
+        },
+      ],
+
+      include: path.resolve(__dirname, "../"),
+    });
+
+    return config;
+  },
 };
