3a3f8872756e707e18fc3dc58fef6db8ccad402e
AdrianLopezGue <adrian.lopez.gue@hotmail.com>
2020-07-15 17:32:09 +0200

    Start navbar creation

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const NavBar: React.FunctionComponent = () => {
+  return (
+    <div className="relative bg-white">
+
+    </div>
+  );
+};
+
+export default NavBar;
