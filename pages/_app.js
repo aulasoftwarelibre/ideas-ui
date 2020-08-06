0834f5a697f223409d28a067badcca1e31ead2ec
Sergio Gómez <sergio@uco.es>
2020-08-06 07:01:29 +0200

    Load session from Client API

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,24 @@
 import '../styles/index.css';
 
-import { getSession, Provider } from 'next-auth/client';
-import App from 'next/app';
+import { Provider } from 'next-auth/client';
 import Head from 'next/head';
 
 import Layout from '../components/Layout/Layout';
 import { NavBarProvider } from '../state/navbar/NavBarProvider';
 
-function MyApp({ Component, session, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
-    <Provider options={{ site: process.env.SITE }} session={pageProps.session}>
+    <Provider session={pageProps.session}>
       <NavBarProvider>
         <Head>
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <Layout session={pageProps.session}>
+        <Layout>
           <Component {...pageProps} />
         </Layout>
       </NavBarProvider>
     </Provider>
   );
 }
-
-MyApp.getInitialProps = async (appContext) => {
-  const appProps = await App.getInitialProps(appContext);
-  return { ...appProps, session: await getSession(appContext) };
-};
 
 export default MyApp;
