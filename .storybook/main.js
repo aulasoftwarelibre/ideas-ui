16b3e9f34ffa02f1bad7097699fd09c45f81ff8f
Sergio Gómez <sergio@uco.es>
2020-08-17 13:53:19 +0200

    Upgrade to storybook 6

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,6 @@ const path = require("path");
 module.exports = {
   stories: ["../components/**/*.stories.tsx"],
   addons: [
-    "@storybook/preset-typescript",
     "@storybook/addon-links",
     "@storybook/addon-knobs",
     "@storybook/addon-actions",
