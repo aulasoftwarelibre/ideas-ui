84636e91bcfaf1e1a2489da53e62cfa6e866fba6
Sergio Gómez <sergio@uco.es>
2020-08-20 17:42:10 +0200

    Change moment format

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,7 +3,7 @@ import '../styles/index.css';
 import Moment from 'react-moment';
 
 Moment.globalLocale = "es";
-Moment.globalFormat = "LLLL";
+Moment.globalFormat = "llll";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
