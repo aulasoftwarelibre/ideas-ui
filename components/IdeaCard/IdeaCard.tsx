04d99f35111a5858919dfa20d73f83e701058ba0
Sergio Gómez <sergio@uco.es>
2020-08-20 17:41:18 +0200

    Add IdeaCardList component and stories

--- components/IdeaCard/IdeaCard.tsx
+++ components/IdeaCard/IdeaCard.tsx
@@ -1,4 +1,3 @@
-import classnames from 'classnames';
 import Link from 'next/link';
 import React from 'react';
 import Moment from 'react-moment';
@@ -15,43 +14,39 @@ export const IdeaCard: React.FunctionComponent<Props> = ({ idea }) => {
 
   return (
     <div
-      className="h-auto max-w-sm rounded-large overflow-hidden shadow-lg"
+      className="relative h-auto max-w-sm rounded-large overflow-hidden shadow-lg"
       data-testid="idea-card"
     >
       <Link href={`/ideas/${idea.slug}`}>
         <a>
           <div
-            className="flex h-56 bg-cover bg-center"
+            className="flex flex-col content-end h-56 bg-cover bg-center"
             style={{
               backgroundImage: `linear-gradient(to bottom,rgba(0, 0, 0, 0),rgba(0, 0, 0, 1)), url(${ideaImage})`,
             }}
           >
-            <div className="p-4 flex-col items-end text-white mt-auto">
-              <h4 className="font-bold text-lightgray text-base">
-                {idea.category}
-              </h4>
-              <h3 className="font-bold text-white text-2xl">{idea.title}</h3>
-            </div>
-            <div className="flex-col items-end text-white mb-auto ml-auto">
+            {idea.online && (
               <div
                 data-testid="idea-type"
-                className={classnames(
-                  "h-8 px-4 py-1 bg-gradient-to-r from-primary to-primary-light rounded-tr-large rounded-bl-large",
-                  { "w-26": idea.online },
-                  { "w-30": !idea.online }
-                )}
+                className="absolute top-0 right-0 h-8 px-4 py-1 w-auto bg-gradient-to-r from-primary to-primary-light rounded-tr-large rounded-bl-large"
               >
-                <p className="font-bold text-white text-l">
-                  {idea.online ? "ONLINE" : "PRESENCIAL"}
+                <p className="font-bold text-white text-xs font-uppercase">
+                  ONLINE
                 </p>
               </div>
+            )}
+            <div className="p-4 items-end text-white mt-auto">
+              <h4 className="font-bold text-white text-opacity-75 text-base">
+                {idea.category}
+              </h4>
+              <h3 className="font-bold text-white text-xl">{idea.title}</h3>
             </div>
           </div>
           <div className="px-5 py-4 bg-gradient-to-r from-primary to-primary-light">
-            <div className="font-bold text-white text-xl capitalize-date">
+            <div className="font-bold text-white text-lg capitalize-date">
               <Moment>{idea.startsAt}</Moment>
             </div>
-            <div className="font-bold text-white text-sm mb-2">
+            <div className="font-bold text-white text-opacity-75 text-sm mb-2">
               {idea.group}
             </div>
             <IdeaAvatarList attendees={idea.attendees}></IdeaAvatarList>
---
+++ components/IdeaCard/IdeaCardList.stories.tsx
@@ -0,0 +1,51 @@
+import faker, { fake } from 'faker';
+import React from 'react';
+
+import { Idea } from '../../model/idea/idea';
+import IdeaCardList, { Props } from './IdeaCardList';
+
+export default {
+  component: IdeaCardList,
+  title: "IdeaCard/IdeaCardList",
+  argTypes: {
+    width: {
+      control: {
+        type: "range",
+        min: 0,
+        max: 8,
+        step: 1,
+      },
+    },
+  },
+};
+
+const ideas: Idea[] = [...Array(8)].map(() => {
+  const title = faker.lorem.sentence();
+  return {
+    id: faker.random.uuid(),
+    slug: faker.helpers.slugify(title),
+    title: title,
+    group: faker.random.arrayElement(["Group A", "Group B", "Group C"]),
+    category: faker.random.arrayElement([
+      "Category A",
+      "Category B",
+      "Category C",
+    ]),
+    image: faker.image.technics(),
+    startsAt: faker.date.future(),
+    online: faker.random.boolean(),
+    attendees: [],
+  };
+});
+
+const Template = (args: Props) => <IdeaCardList {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  ideas: ideas,
+};
+
+export const Empty = Template.bind({});
+Empty.args = {
+  ideas: [],
+};
