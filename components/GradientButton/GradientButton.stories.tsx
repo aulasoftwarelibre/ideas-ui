71a61e5343edcb8c6cdb5f5eaa810c10da7f62c7
Sergio Gómez <sergio@uco.es>
2020-08-20 12:22:33 +0200

    Fix GradientButton design

--- components/GradientButton/GradientButton.stories.tsx
+++ components/GradientButton/GradientButton.stories.tsx
@@ -1,11 +1,9 @@
-import { action } from '@storybook/addon-actions';
-import { text } from '@storybook/addon-knobs';
 import React from 'react';
 
 import GradientButton, { Props } from './GradientButton';
 
 export default {
-  title: "Gradient Button",
+  title: "Components/GradientButton",
   component: GradientButton,
   argTypes: {
     children: { control: "text" },
--- components/GradientButton/GradientButton.tsx
+++ components/GradientButton/GradientButton.tsx
@@ -10,7 +10,7 @@ export const GradientButton: React.FunctionComponent<Props> = ({
 }) => {
   return (
     <button
-      className="ml-3 bg-gradient-to-r from-primary to-primary-light hover:bg-none hover:bg-secondary text-white font-bold py-2 px-6 text-sm rounded-md"
+      className="ml-3 bg-gradient-to-r from-primary to-primary-light hover:bg-none hover:bg-secondary text-white font-bold py-2 px-6 text-sm rounded-md shadow-md"
       onClick={() => onClick()}
       data-testid="gradient-button"
     >
