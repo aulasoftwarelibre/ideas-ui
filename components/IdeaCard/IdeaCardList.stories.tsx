fe49daeb07cfa7da6dabe3da753eb01c128f04e9
AdrianLopezGue <adrian.lopez.gue@hotmail.com>
2020-08-21 11:21:06 +0200

    Modify IdeaCardList to remove fixtures

--- components/IdeaCard/IdeaCardList.stories.tsx
+++ components/IdeaCard/IdeaCardList.stories.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import { ideas } from './__fixtures__/ideas';
-import IdeaCardList, { Props } from './IdeaCardList';
+import IdeaCardList, { Props } from "./IdeaCardList";
+import faker from "faker";
+import { Idea } from "../../model/idea/idea";
+
+export const ideas: Idea[] = [...Array(8)].map(() => {
+  const title = faker.lorem.sentence();
+  return {
+    id: faker.random.uuid(),
+    slug: faker.helpers.slugify(title),
+    title: title,
+    group: faker.random.arrayElement(["Group A", "Group B", "Group C"]),
+    category: faker.random.arrayElement([
+      "Category A",
+      "Category B",
+      "Category C",
+    ]),
+    image: faker.image.technics(),
+    startsAt: faker.date.future(),
+    online: faker.random.boolean(),
+    attendees: [],
+  };
+});
 
 export default {
   component: IdeaCardList,
   title: "IdeaCard/IdeaCardList",
+  includeStories: ["Default", "Empty"],
 };
 
 const Template = (args: Props) => <IdeaCardList {...args} />;
