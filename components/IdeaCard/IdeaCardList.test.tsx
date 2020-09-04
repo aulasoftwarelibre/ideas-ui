66259e0d13b79137d8601b967a8d9b36e4671a17
Sergio Gómez <sergio@uco.es>
2020-09-04 23:41:42 +0200

    Update IdeaCard

--- components/IdeaCard/IdeaCardList.test.tsx
+++ components/IdeaCard/IdeaCardList.test.tsx
@@ -1,16 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 
+import { Props } from './IdeaCardList';
 import { Default, Empty } from './IdeaCardList.stories';
 
 describe("IdeCardList", () => {
   it("should show a warning when list is empty", () => {
-    render(<Empty {...Empty.args} />);
+    render(<Empty {...(Empty.args as Props)} />);
     expect(screen.getByTestId("not-found"));
   });
 
   it("should show all ideas", () => {
-    render(<Default {...Default.args} />);
+    render(<Default {...(Default.args as Props)} />);
     expect(screen.getAllByTestId("idea-card")).toHaveLength(8);
   });
 });
