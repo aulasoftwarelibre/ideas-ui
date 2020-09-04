cda0e683f44e2f9fb94e1bb05e7a161c45eef093
Sergio Gómez <sergio@uco.es>
2020-09-04 23:27:04 +0200

    Update Footer

--- components/Footer/Footer.test.tsx
+++ components/Footer/Footer.test.tsx
@@ -1,11 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 
+import { Props } from './Footer';
 import { Default } from './Footer.stories';
 
 describe("Footer", () => {
   it("should render footer", () => {
-    render(<Default {...Default.args} />);
+    render(<Default {...(Default.args as Props)} />);
     expect(screen.getByTestId("footer"));
   });
 });
